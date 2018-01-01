5cc0ed124a56319bfba3cbd6f53f6d705adddfc2
Alan <ivda.calderon@gmail.com>
2018-01-01 15:45:55 -0300

    project

---
+++ Models/CotizacionesContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Cotizaciones.Models;
+
+namespace Cotizaciones.Data{
+
+    public class CotizacionesContext : DbContext{
+
+        public CotizacionesContext(DbContextOptions<CotizacionesContext> options) : base(options){
+
+
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+
+            optionsBuilder.UseSqlite("Data Source = cotizaciones.db");
+        }
+
+        public DbSet <Persona> Personas { get; set; } 
+
+
+    }
+
+}
